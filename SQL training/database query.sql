dbc8b7b989a706f82470ddbccc694146fe319023
Alicja Sawicka <161524049+alicjasaw@users.noreply.github.com>
2024-09-06 01:22:16 +0200

    adding values to tables

--- SQL training/database query.sql
+++ SQL training/database query.sql
@@ -16,3 +16,31 @@ CREATE TABLE table.dane (
     kwota transferu int,
     poprzedni_klub_kraj varchar(25)
 );
+
+// adding data to table.team
+INSERT into table.team
+VALUES
+('1','BR','Thibaut Courtois', '28', '0'),
+('13','BR','Andriy Lunin', '21', '2'),
+('4','O','Sergio Ramos', '34', '0'),
+('6','O','Nacho Fernandez', '30', '2'),
+('3','O', 'Eder Militao', '22', '3'),
+('8','P','Toni Kroos', '30', '1'),
+('15','P','Fede Valverde', '22', '0'),
+('14','P','Casemiro', '28', '2'),
+('9','N','Karim Benzema', '32', '1'),
+('18','N','Luka Jovic', '22', '3');
+
+// adding data to table.dane
+INSERT INTO table.dane
+VALUES
+('1', 'Belgia', 'x', '75', '35', 'Anglia'),
+('13', 'Ukraina', 'x', '3', '9', 'Ukraina'),
+('4', 'Hiszpania', 'x', '14', '27', 'Hiszpania'),
+('6', 'Hiszpania', '', '10', '0', 'wychowanek'),
+('3', 'Brazylia', 'x', '40', '50', 'Portugalia'),
+('8', 'Niemcy', 'x', '65', '25', 'Niemcy'),
+('15', 'Urugwaj', 'x', '70', '0', 'wychowanek'),
+('14', 'Brazylia', 'x', '50', '0', 'wychowanek'),
+('9', 'Francja', '', '25', '35', 'Francja'),
+('18', 'Serbia', 'x', '22', '63', 'Niemcy');
