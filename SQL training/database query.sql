76addd47fcc141a394ad94631724a31d1009e072
Alicja Sawicka <161524049+alicjasaw@users.noreply.github.com>
2024-09-06 13:14:02 +0200

    Update database query.sql

--- SQL training/database query.sql
+++ SQL training/database query.sql
@@ -1,46 +1,46 @@
 // create table.team
 CREATE TABLE table.team (
-    numer_koszulki int FOREIGN KEY REFERENCES table.dane(numer_koszulki),
-    pozycja varchar(5),
-    imie_nazwisko varchar (35) PRIMARY KEY,
-    wiek int,
-    szansa_transfer int
+    player_number int FOREIGN KEY REFERENCES table.dane(player_number),
+    role varchar(5),
+    player_name varchar (35) PRIMARY KEY,
+    age int,
+    transfer_chance int
 );
 
 // create table.dane
 CREATE TABLE table.dane (
-    numer_koszulki int PRIMARY KEY,
-    kraj_pochodzenia varchar(25),
-    reprezentant kraju varchar(1),
-    wartosc_rynkowa int,
-    kwota transferu int,
-    poprzedni_klub_kraj varchar(25)
+    player_number int PRIMARY KEY,
+    country varchar(25),
+    country_representatives varchar(1),
+    market_value int,
+    transfer_amount int,
+    previous_club_country varchar(25)
 );
 
 // adding data to table.team
 INSERT into table.team
 VALUES
-('1','BR','Thibaut Courtois', '28', '0'),
-('13','BR','Andriy Lunin', '21', '2'),
-('4','O','Sergio Ramos', '34', '0'),
-('6','O','Nacho Fernandez', '30', '2'),
-('3','O', 'Eder Militao', '22', '3'),
-('8','P','Toni Kroos', '30', '1'),
-('15','P','Fede Valverde', '22', '0'),
-('14','P','Casemiro', '28', '2'),
-('9','N','Karim Benzema', '32', '1'),
-('18','N','Luka Jovic', '22', '3');
+('1','GK','Thibaut Courtois', '28', '0'),
+('13','GK','Andriy Lunin', '21', '2'),
+('4','D','Sergio Ramos', '34', '0'),
+('6','D','Nacho Fernandez', '30', '2'),
+('3','D', 'Eder Militao', '22', '3'),
+('8','M','Toni Kroos', '30', '1'),
+('15','M','Fede Valverde', '22', '0'),
+('14','M','Casemiro', '28', '2'),
+('9','F','Karim Benzema', '32', '1'),
+('18','F','Luka Jovic', '22', '3');
 
 // adding data to table.dane
 INSERT INTO table.dane
 VALUES
-('1', 'Belgia', 'x', '75', '35', 'Anglia'),
-('13', 'Ukraina', 'x', '3', '9', 'Ukraina'),
-('4', 'Hiszpania', 'x', '14', '27', 'Hiszpania'),
-('6', 'Hiszpania', '', '10', '0', 'wychowanek'),
-('3', 'Brazylia', 'x', '40', '50', 'Portugalia'),
-('8', 'Niemcy', 'x', '65', '25', 'Niemcy'),
-('15', 'Urugwaj', 'x', '70', '0', 'wychowanek'),
-('14', 'Brazylia', 'x', '50', '0', 'wychowanek'),
-('9', 'Francja', '', '25', '35', 'Francja'),
-('18', 'Serbia', 'x', '22', '63', 'Niemcy');
+('1', 'Belgium', 'x', '75', '35', 'England'),
+('13', 'Ukraine', 'x', '3', '9', 'Ukraine'),
+('4', 'Spain', 'x', '14', '27', 'Spain'),
+('6', 'Spain', '', '10', '0', 'alumnus'),
+('3', 'Brazil', 'x', '40', '50', 'Portugal'),
+('8', 'Germany', 'x', '65', '25', 'Germany'),
+('15', 'Uruguay', 'x', '70', '0', 'alumnus'),
+('14', 'Brazil', 'x', '50', '0', 'alumnus'),
+('9', 'France', '', '25', '35', 'France'),
+('18', 'Serbia', 'x', '22', '63', 'Germany');
